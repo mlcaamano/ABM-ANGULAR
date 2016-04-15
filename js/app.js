44444ca254198488a43a15b6435d97c2c1c879fd
mlcaamano <matias_caamano2004@yahoo.com.ar>
2016-04-15 15:17:47 -0300

    Clase 14/04/2016 falta el modificar

---
+++ js/app.js
@@ -0,0 +1,116 @@
+
+var app = angular.module('ABMangularPHP', []);
+
+
+app.controller('controlMenu', function($scope, $http) {
+  $scope.DatoTest="**Menu**";
+});
+
+
+app.controller('controlAlta', function($scope, $http) {
+  $scope.DatoTest="**alta**";
+
+//inicio las variables
+  $scope.persona={};
+  $scope.persona.nombre= "natalia" ;
+  $scope.persona.dni= "12312312" ;
+  $scope.persona.apellido= "natalia" ;
+  $scope.persona.foto="sinfoto";
+
+
+  $scope.Guardar=function(){
+
+
+  	console.log("persona a guardar:");
+    console.log($scope.persona);
+    $http.post('PHP/nexo.php', { datos: {accion :"insertar",persona:$scope.persona}})
+ 	  .then(function(respuesta) {     	
+ 		     //aca se ejetuca si retorno sin errores      	
+      	 console.log(respuesta.data);
+
+    },function errorCallback(response) {     		
+     		//aca se ejecuta cuando hay errores
+     		console.log( response);     			
+ 	  });
+
+  
+
+  }
+});
+
+
+app.controller('controlGrilla', function($scope, $http) {
+  	$scope.DatoTest="**grilla**";
+ 	
+    function bien(respuesta)
+    {
+        $scope.ListadoPersonas = respuesta.data.listado;
+        console.log(respuesta.data);
+    }
+
+    function mal(respuesta)
+    {
+        $scope.ListadoPersonas= [];
+        console.log( response);
+    }
+
+  $http.get('PHP/nexo.php', { params: {accion :"traer"}})
+  .then(bien, mal);
+
+ 	$scope.Borrar=function(persona){
+		console.log("borrar"+persona);
+
+
+
+$http.post("PHP/nexo.php",{datos:{accion :"borrar",persona:persona}},{headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+ .then(function(respuesta) {       
+         //aca se ejetuca si retorno sin errores        
+         console.log(respuesta.data);
+
+
+        $http.get('PHP/nexo.php', { params: {accion :"traer"}})
+  .then(function(respuesta) {       
+
+         $scope.ListadoPersonas = respuesta.data.listado;
+         console.log(respuesta.data);
+
+    },function errorCallback(response) {
+         $scope.ListadoPersonas= [];
+        console.log( response);
+   });
+
+
+    },function errorCallback(response) {        
+        //aca se ejecuta cuando hay errores
+        console.log( response);           
+    });
+
+/*
+     $http.post('PHP/nexo.php', 
+      headers: 'Content-Type': 'application/x-www-form-urlencoded',
+      params: {accion :"borrar",persona:persona})
+    .then(function(respuesta) {       
+         //aca se ejetuca si retorno sin errores        
+         console.log(respuesta.data);
+
+    },function errorCallback(response) {        
+        //aca se ejecuta cuando hay errores
+        console.log( response);           
+    });
+
+*/
+ 	}
+
+
+
+
+ 	$scope.Modificar=function(id){
+ 		
+ 		console.log("Modificar"+id);
+ 	}
+
+
+
+
+
+});
